bb988250f37675609c14c37a5732cb7c0519b209
Kayla Durrett <kayla.durrett@gmail.com>
2022-03-01 16:45:45 -0700

    Add inital styling to main page

---
+++ src/components/ChillLogLink.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../styles/ChillLogLink.css'
+
+const ChillLogLink = () => {
+  return (
+    <section className='chill-log-link'>
+      <h2 className='chill-log-title'>Chill Log</h2>
+    </section>
+  )
+}
+
+export default ChillLogLink 
