443e9065c0ef4f90e463c4f557ee7444cd10d5e6
Kayla Durrett <kayla.durrett@gmail.com>
2022-03-02 15:55:29 -0700

    Add method to clear selected feeling on main page

--- src/components/App.js
+++ src/components/App.js
@@ -23,19 +23,23 @@ class App extends Component {
       .then(data => this.setState({ selectedFeeling: data}))
   }
 
+  resetSelectedFeeling = () => {
+    this.setState({ selectedFeeling: ''})
+  }
+
   render() {
     return (
       <section className='App'>
         <Switch>
           <Route exact path='/'>
-            <MainNav setSelectedFeeling={this.setSelectedFeeling}/>
+            <MainNav setSelectedFeeling={this.setSelectedFeeling} resetSelectedFeeling={this.resetSelectedFeeling}/>
             {this.state.selectedFeeling ? <ActionsContainer selectedFeeling={this.state.selectedFeeling}/> : null}
           </Route>
           <Route exact path='/:feelingId/:actionId/action_detail' render={({ match }) => {
-            return [<DetailNav key={match.params.actionId + 'detail_nav'} actionId={match.params.actionId} feelingId={match.params.feelingId} />, <ActionDetailContainer id={match.params.actionId} key={match.params.actionId + 'action_detail'}/>]
+            return [<DetailNav key={match.params.actionId + 'detail_nav'} actionId={match.params.actionId} feelingId={match.params.feelingId} resetSelectedFeeling={this.resetSelectedFeeling} />, <ActionDetailContainer id={match.params.actionId} key={match.params.actionId + 'action_detail'}/>]
           }} />
           <Route exact path='/chill_log' render={() => {
-            return [<Nav key='nav' />, <ChillLogContainer key='chill_log' />]
+            return [<Nav key='nav' resetSelectedFeeling={this.resetSelectedFeeling} />, <ChillLogContainer key='chill_log' />]
           }}/>
         </Switch>
       </section>
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,10 +2,10 @@ import React, {Component} from 'react'
 import '../styles/Nav.css'
 import { Link } from 'react-router-dom'
 
-const Nav = () => {
+const Nav = ({ resetSelectedFeeling }) => {
     return (
       <section className='nav-bar'>
-        <Link to='/' className='nav-title'>Let's Chill</Link>
+        <Link to='/' className='nav-title' onClick={resetSelectedFeeling}>Let's Chill</Link>
       </section>
     )
 }
