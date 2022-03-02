188fd3367ce9f67a9c2e2485e662a3ff08188263
Kayla Durrett <kayla.durrett@gmail.com>
2022-03-01 17:12:19 -0700

    Create initial api calls

---
+++ src/apiCalls.js
@@ -0,0 +1,34 @@
+export const getAllFeelings = () => {
+  return fetch('https://lets-chill-api.herokuapp.com/api/v1/feelings')
+    .then(response => response.json())
+}
+
+export const getSpecificFeeling = id => {
+  return fetch(`https://lets-chill-api.herokuapp.com/api/v1/feelings/${id}`)
+    .then(response => response.json())
+}
+
+export const getAllActions = () => {
+  return fetch('https://lets-chill-api.herokuapp.com/api/v1/actions')
+    .then(response => response.json())
+}
+
+export const getSpecificAction = id => {
+  return fetch(`https://lets-chill-api.herokuapp.com/api/v1/actions/${id}`)
+    .then(response => response.json())
+}
+
+export const getLog = () => {
+  return fetch('https://lets-chill-api.herokuapp.com/api/v1/log')
+    .then(response => response.json())
+}
+
+export const postToLog = (entry) => {
+  return fetch('https://lets-chill-api.herokuapp.com/api/v1/log', {
+    method: 'POST',
+    body: JSON.stringify(entry),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }).then(response => response.json())
+}
