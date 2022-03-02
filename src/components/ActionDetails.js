bf2334b3bc27b5b62c3b6f279bcd622022e07e02
Kayla Durrett <kayla.durrett@gmail.com>
2022-03-02 09:20:58 -0700

    Add api get request to obtain and display action details

--- src/components/ActionDetails.js
+++ src/components/ActionDetails.js
@@ -1,17 +1,28 @@
 import React, { Component } from 'react'
+import { getSpecificAction, getSpecificFeeling } from '../apiCalls'
 import '../styles/ActionDetails.css'
 
 class ActionDetails extends Component {
   constructor(props) {
-    super()
+    super(props)
     this.state = {
       action: ''
     }
   }
 
+  componentDidMount = () => {
+    getSpecificAction(parseInt(this.props.id))
+      .then(data => this.setState({ action: data}))
+  }
+
   render() {
     return (
-      <h2>These are action details</h2>
+      <section className='action-detail-card'>
+        <h2>{this.state.action.action}</h2>
+        <p>{this.state.action.description}</p>
+        <img src={this.state.action.img} alt={`${this.state.action.action} gif`}/>
+        <a href={this.state.action.link} target="_blank" rel="noopener noreferrer">Reference Link</a>
+      </section>
     )
   }
 }
